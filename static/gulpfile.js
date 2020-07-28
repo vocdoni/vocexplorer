6718d09d87edba33604056b8035b5916887296e9
Òscar Casajuana <elboletaire@gmail.com>
2020-07-28 18:22:51 +0200

    refs #3 Added go generate watch method in gulp

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -1,7 +1,16 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const exec = require('child_process').exec
 
 sass.compiler = require('node-sass')
+
+gulp.task('go:generate', (done) =>
+  exec('go generate', {cwd: '../frontend'}, done)
+)
+
+gulp.task('go:watch', () =>
+  gulp.watch('../frontend/**/*.go', gulp.series('go:generate'))
+)
 
 gulp.task('sass', () =>
   gulp.src('./sass/**/*.scss')
