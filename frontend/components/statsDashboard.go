e67453ee7eb64a892195140253800ae713a915a4
NateWilliams2 <Williams2@grinnell.edu>
2020-09-01 10:58:05 -0400

    Move stats to standalone view

---
+++ frontend/components/statsDashboard.go
@@ -0,0 +1,66 @@
+package components
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gopherjs/vecty"
+	"gitlab.com/vocdoni/vocexplorer/frontend/actions"
+	"gitlab.com/vocdoni/vocexplorer/frontend/api"
+	"gitlab.com/vocdoni/vocexplorer/frontend/bootstrap"
+	"gitlab.com/vocdoni/vocexplorer/frontend/dispatcher"
+	"gitlab.com/vocdoni/vocexplorer/frontend/store"
+	"gitlab.com/vocdoni/vocexplorer/rpc"
+)
+
+// StatsDashboardView renders the dashboard landing page
+type StatsDashboardView struct {
+	vecty.Core
+	vecty.Mounter
+	Rendered bool
+}
+
+// Mount is called after the component renders to signal that it can be rerendered safely
+func (dash *StatsDashboardView) Mount() {
+	dash.Rendered = true
+}
+
+// Render renders the StatsDashboardView component
+func (dash *StatsDashboardView) Render() vecty.ComponentOrHTML {
+	if dash != nil {
+		return Container(
+			renderGatewayConnectionBanner(),
+			renderServerConnectionBanner(),
+			&BlockchainInfo{},
+		)
+	}
+	return &bootstrap.Alert{
+		Contents: "Connecting to blockchain clients",
+		Type:     "warning",
+	}
+}
+
+// UpdateAndRenderStatsDashboard keeps the stats dashboard updated
+func UpdateAndRenderStatsDashboard(d *StatsDashboardView) {
+	actions.EnableUpdates()
+	ticker := time.NewTicker(time.Duration(store.Config.RefreshTime) * time.Second)
+	updateStatsDashboard(d)
+	for {
+		select {
+		case <-store.RedirectChan:
+			fmt.Println("Redirecting...")
+			ticker.Stop()
+			return
+		case <-ticker.C:
+			updateStatsDashboard(d)
+		}
+	}
+}
+
+func updateStatsDashboard(d *StatsDashboardView) {
+	dispatcher.Dispatch(&actions.GatewayConnected{Connected: api.PingGateway(store.Config.GatewayHost)})
+	dispatcher.Dispatch(&actions.ServerConnected{Connected: api.Ping()})
+
+	actions.UpdateCounts()
+	rpc.UpdateBlockchainStatus(store.TendermintClient)
+}
--- frontend/pages/stats.go
+++ frontend/pages/stats.go
@@ -2,10 +2,9 @@ package pages
 
 import (
 	"github.com/gopherjs/vecty"
-	"github.com/gopherjs/vecty/elem"
 	"gitlab.com/vocdoni/vocexplorer/config"
-	"gitlab.com/vocdoni/vocexplorer/frontend/bootstrap"
 	"gitlab.com/vocdoni/vocexplorer/frontend/components"
+	"gitlab.com/vocdoni/vocexplorer/frontend/store"
 )
 
 // Stats is a pretty page for all our blockchain statistics
@@ -21,17 +20,14 @@ func (stats *Stats) Render() vecty.ComponentOrHTML {
 
 // Component returns the stats component
 func (stats *Stats) Component() vecty.ComponentOrHTML {
-	return components.Container(
-		elem.Section(
-			bootstrap.Card(bootstrap.CardParams{
-				Body: vecty.List{
-					elem.Heading3(
-						vecty.Text("Stats"),
-					),
-					&components.BlockchainInfo{},
-					vecty.Text("Most stats will be moved here :)"),
-				},
-			}),
-		),
-	)
+	dash := new(components.StatsDashboardView)
+	dash.Rendered = false
+	// Ensure component rerender is only triggered once component has been rendered
+	store.Listeners.Add(dash, func() {
+		if dash.Rendered {
+			vecty.Rerender(dash)
+		}
+	})
+	go components.UpdateAndRenderStatsDashboard(dash)
+	return dash
 }
